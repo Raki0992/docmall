fd16cc3b10221372e4b278ebc7c96c5cec71f57e
Raki0992 <kimsangkyoon09@naver.com>
2023-11-01 17:52:01 +0900

    DB에 맞춰서 이름 수정

--- src/main/java/com/docmall/domain/CategoryVO.java
+++ src/main/java/com/docmall/domain/CategoryVO.java
@@ -6,36 +6,40 @@ package com.docmall.domain;
 public class CategoryVO {
 
 	private Integer cg_code;	// 모든 카테고리 코드
-	private Integer cg_parent;	// 1차 카테고리 코드
+	private Integer cg_parent_code;	// 1차 카테고리 코드
 	private String  cg_name;	 
 	
 	// @Getter, @Setter
 	public Integer getCg_code() {
 		return cg_code;
 	}
+
 	public void setCg_code(Integer cg_code) {
 		this.cg_code = cg_code;
 	}
-	public Integer getCg_parent() {
-		return cg_parent;
+
+	public Integer getCg_parent_code() {
+		return cg_parent_code;
 	}
-	public void setCg_parent(Integer cg_parent) {
-		this.cg_parent = cg_parent;
+
+	public void setCg_parent_code(Integer cg_parent_code) {
+		this.cg_parent_code = cg_parent_code;
 	}
+
 	public String getCg_name() {
 		return cg_name;
 	}
+
 	public void setCg_name(String cg_name) {
 		this.cg_name = cg_name;
 	}
+
 	
 	// @ToString
 	@Override
 	public String toString() {
-		return "CategoryVO [cg_code=" + cg_code + ", cg_parent=" + cg_parent + ", cg_name=" + cg_name + "]";
+		return "CategoryVO [cg_code=" + cg_code + ", cg_parent_code=" + cg_parent_code + ", cg_name=" + cg_name + "]";
 	}
-	
-	
 	
 	
 }
